3ef6f5a669c0dca95aff95736677abbbd57eb8bc
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-01-24 11:28:09 -0300

    Theme file and initial design of the application

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
+
+    const { top } = useSafeAreaInsets();
+
     return (
-        <View>
-            <Text> Home Screen </Text>
-        </View>
+        <>
+            <Image 
+                source={ require('../assets/pokebola.png') }
+                style={ styles.pokeballBg }
+            />
+            <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20  }}> Pokedex </Text>
+        </>
     );
 };
