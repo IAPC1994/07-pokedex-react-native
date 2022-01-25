cbd8c5dcbd17725df5138500fd25660dfae9a549
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-01-24 22:31:14 -0300

    Axios installed and call of the pokemon api for get the pokemons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Image, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text } from "react-native";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
+    const { simplePokemonList, loadPokemons } = usePokemonPaginated();
 
     return (
         <>
@@ -13,7 +15,33 @@ export const HomeScreen = () => {
                 source={ require('../assets/pokebola.png') }
                 style={ styles.pokeballBg }
             />
-            <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20  }}> Pokedex </Text>
+            {/* <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20  }}> Pokedex </Text> */}
+            <FlatList 
+                data={ simplePokemonList }
+                keyExtractor={ (pokemon) => pokemon.id }
+                renderItem={ ({ item }) => (
+                    <Image 
+                        source={{ uri: item.picture }}
+                        style={{
+                            width: 100,
+                            height: 100,
+                        }}
+                    />
+                )}
+
+                showsVerticalScrollIndicator={ false }
+
+                onEndReached={ loadPokemons }
+                onEndReachedThreshold={ 0.4 }
+
+                ListFooterComponent={( 
+                    <ActivityIndicator 
+                        style={{ height: 100 }}
+                        size={ 20 }
+                        color='grey'
+                    />
+                )}
+            />
         </>
     );
 };
