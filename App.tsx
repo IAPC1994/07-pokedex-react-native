c8ba58df462808694bb7d70a10e55a5390c1518a
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-01-25 03:30:16 -0300

    Search Screen working and update on navigator

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import { LogBox } from 'react-native';
-import { Navigator } from './src/router/Navigator';
+// import { Navigator } from './src/router/Navigator';
+import { Tabs } from './src/router/Tabs';
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
@@ -11,7 +12,7 @@ LogBox.ignoreLogs([
 const App = () => {
     return (
       <NavigationContainer>
-        <Navigator />
+        <Tabs />
       </NavigationContainer>
     );
 };
